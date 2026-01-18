2b64a6389311b36576e00ea1c3e4ffe3c18dba73
t.hirabuki <t.hirabuki@gmail.com>
2026-01-18 07:39:34 +0000

    Try deploy with production config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
-import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {};
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development", // 開発中は無効、本番は有効
+});
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ここに設定を追加できます
+};
+
+export default withPWA(nextConfig);
